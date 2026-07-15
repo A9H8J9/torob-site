c9433d991cac11f71d1356349893e4061e1bb023
amirhosein <140063353+A9H8J9@users.noreply.github.com>
2026-07-15 17:19:56 +0330

    add api to search landing

--- app/Hero.tsx
+++ app/Hero.tsx
@@ -3,78 +3,43 @@
 import { Camera, Search, History } from "lucide-react";
 import { useEffect, useRef, useState } from "react";
 
-const suggestions = [
-  {
-    text: "گوشی تبلت",
-    is_history: true,
-  },
-  {
-    text: "گوشی س",
-    is_history: true,
-  },
-  {
-    text: "گوشی مینی اسمارت استور",
-    suggestion_type: "internet_shop",
-    suggestion_group: "entity",
-    is_history: false,
-    entity: {
-      type: "internet_shop",
-      id: 296438,
-      title: "گوشی مینی اسمارت استور",
-      subtitle: "smartstoree.ir",
-      logo_url:
-        "https://storage3.torob.com/backend-api/internet_shop/logos/05a922718c5d.png",
-    },
-  },
-  {
-    text: "گوشی پلازا",
-    suggestion_type: "internet_shop",
-    suggestion_group: "entity",
-    is_history: false,
-    entity: {
-      type: "internet_shop",
-      id: 8114,
-      title: "گوشی پلازا",
-      subtitle: "gooshiplaza.com",
-      logo_url:
-        "https://storage3.torob.com/backend-api/internet_shop/logos/603b8eab7dc9.png",
-    },
-  },
-  {
-    text: "گوشی سامسونگ",
-    is_history: false,
-  },
-  {
-    text: "گوشی شیایومی",
-    is_history: false,
-  },
-  {
-    text: "گوشی ایفون",
-    is_history: false,
-  },
-];
+import { useGetAutoComplete } from "@/lib/apis";
+
+
+type Suggestion = {
+  type: "text" | "shop";
+  text: string;
+  logo?: string;
+  is_history: boolean;
+};
 
 
 export default function Hero() {
+
   const [query, setQuery] = useState("");
   const [open, setOpen] = useState(false);
 
   const wrapperRef = useRef<HTMLDivElement>(null);
 
 
-  const filteredSuggestions = suggestions.filter((item) =>
-    item.text.includes(query)
-  );
+  const {
+    data: suggestions = [],
+    isPending,
+  } = useGetAutoComplete(query);
+
 
 
   useEffect(() => {
+
     function handleClickOutside(event: MouseEvent) {
+
       if (
         wrapperRef.current &&
         !wrapperRef.current.contains(event.target as Node)
       ) {
         setOpen(false);
       }
+
     }
 
 
@@ -90,53 +55,58 @@ export default function Hero() {
         handleClickOutside
       );
     };
+
   }, []);
 
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+   function handleFileChange(
+    event: React.ChangeEvent<HTMLInputElement>
+  ) {
+
+    const file = event.target.files?.[0];
+
+
+    if(!file) return;
+    console.log(file);
+  } 
 
 
   return (
     <main className="mt-40 flex flex-col items-center justify-center px-6">
-
       <div className="mb-12 flex flex-col items-center">
-
         <div>
           <img
             className="hidden dark:flex"
             src="/icons/logo-dark.svg"
           />
-
           <img
             className="flex dark:hidden"
             src="/icons/logo-light.svg"
           />
         </div>
-
-
         <h1 className="mt-3 text-5xl font-black tracking-tight text-[#e91e33] dark:text-white">
           ترب
         </h1>
-
       </div>
-
-
-
       <div
         ref={wrapperRef}
         className="relative w-full max-w-md"
       >
-
-        <div className="
-          relative
-          h-12
-          w-full
-          rounded-sm
-          border
-          border-gray-400
-          bg-white
-          dark:bg-[#212b36]
-        ">
-
+        <div
+          className="
+            relative
+            h-12
+            w-full
+            rounded-sm
+            border
+            border-gray-400
+            bg-white
+            dark:bg-[#212b36]
+          "
+        >
           <button
+            type="button"
             className="
               absolute
               right-2
@@ -147,16 +117,17 @@ export default function Hero() {
           >
             <Search size={22}/>
           </button>
-
-
           <input
             value={query}
-            onFocus={() => {
-              if(query) setOpen(true);
+            onFocus={()=>{
+              if(query){
+                setOpen(true);
+              }
             }}
-            onChange={(e)=> {
+            onChange={(e)=>{
               setQuery(e.target.value);
               setOpen(true);
+
             }}
             placeholder="نام کالا یا فروشگاه را وارد کنید"
             className="
@@ -170,26 +141,42 @@ export default function Hero() {
               placeholder:text-slate-400
               dark:text-white
             "
+
           />
 
+          <input
+        ref={fileInputRef}
+        type="file"
+        accept="image/*"
+        className="hidden"
+        onChange={handleFileChange}
+      />
 
-          <button
-            className="
-              absolute
-              left-2
-              top-1/2
-              -translate-y-1/2
-              text-slate-400
-            "
-          >
-            <Camera size={22}/>
-          </button>
+
+      <button
+        type="button"
+        onClick={() => fileInputRef.current?.click()}
+        className="
+          absolute
+          cursor-pointer
+          left-2
+          top-1/2
+          -translate-y-1/2
+          text-slate-400
+        "
+      >
+        <Camera size={22}/>
+      </button>
+
 
         </div>
 
 
 
-        {open && query && (
+
+
+        {
+          open && query && (
           <div
             className="
               absolute
@@ -206,18 +193,29 @@ export default function Hero() {
             "
           >
 
-            {filteredSuggestions.length === 0 ? (
 
-              <div className="p-4 text-center text-sm text-slate-400">
-                نتیجه‌ای پیدا نشد
-              </div>
-
-            ) : (
-
-              filteredSuggestions.map((item,index)=> (
+            {
+              isPending ? (
+                <div className="p-4 text-center text-sm text-slate-400">
+                  در حال جستجو...
+                </div>
+              )
+              :
+              suggestions.length === 0 ? (
+                <div className="p-4 text-center text-sm text-slate-400">
+                  نتیجه‌ای پیدا نشد
+                </div>
+              )
+              :
+              suggestions.map(
+                (item: Suggestion, index:number)=> (
 
                 <button
+
                   key={index}
+
+                  type="button"
+
                   className="
                     flex
                     w-full
@@ -229,68 +227,76 @@ export default function Hero() {
                     hover:bg-slate-100
                     dark:hover:bg-slate-800
                   "
+
                 >
 
+
                   {
-                    item.entity ? (
+                    item.type === "shop" ? (
 
-                      <>
-                        <img
-                          src={item.entity.logo_url}
-                          className="
-                            h-8
-                            w-8
-                            rounded-full
-                          "
-                        />
-
-                        <div>
-                          <p className="text-sm dark:text-white">
-                            {item.entity.title}
-                          </p>
-
-                          <p className="text-xs text-slate-400">
-                            {item.entity.subtitle}
-                          </p>
-                        </div>
-                      </>
-
-                    ) : (
-
-                      <>
-                        {
-                          item.is_history ? (
-                            <History size={18}/>
-                          ) : (
-                            <Search size={18}/>
-                          )
-                        }
-
-                        <span className="dark:text-white">
-                          {item.text}
-                        </span>
-                      </>
+                      <img
+                        src={item.logo}
+                        className="
+                          h-8
+                          w-8
+                          rounded-full
+                          object-cover
+                        "
+                      />
 
                     )
+
+                    :
+
+                    item.is_history ? (
+
+                      <History
+                        size={18}
+                        className="text-slate-400"
+                      />
+
+                    )
+
+                    :
+
+                    (
+
+                      <Search
+                        size={18}
+                        className="text-slate-400"
+                      />
+
+                    )
+
                   }
+
+
+
+                  <span
+                    className="
+                      text-sm
+                      dark:text-white
+                    "
+                  >
+                    {item.text}
+                  </span>
+
 
 
                 </button>
 
-              ))
+              )
 
-            )}
-
+              )
+            }
           </div>
-        )}
-
+          )
+        }
       </div>
-
-
-
       <p className="mt-8 text-center text-sm text-[#64748b] dark:text-[#94a3b8]">
         مقایسه قیمت میلیون‌ها محصول بین هزاران فروشگاه
       </p>
+
 
     </main>
   );
