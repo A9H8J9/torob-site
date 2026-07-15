323696436ce91e0b83b6373e1a0894d1bf941f11
amirhosein <140063353+A9H8J9@users.noreply.github.com>
2026-07-15 17:21:32 +0330

    create api.tsx

--- lib/apis.tsx
+++ lib/apis.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { axiosClient } from "./axios";
 
 export function useGetUser() {
@@ -21,12 +21,40 @@ export function useGetUserFavorites() {
   })
 }
 
+export function usePostUserFavorite() {
+    const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: async (data) => {
+      const res = await axiosClient.post('/users/me/favorites/toggle', data)
+      return res.data
+    },
+    onError: console.error,
+    onSuccess: data => {
+      queryClient.invalidateQueries({ queryKey: ['favorites'] })
+    },
+  })
+}
+
 export function useGetUserHistories() {
      return useQuery<any>({
     queryKey: ['histories'],
     queryFn: async () => {
       const res = await axiosClient.get('/users/me/histories')
       return res.data
+    },
+  })
+}
+
+export function useDeleteUserHistories() {
+    const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: async (data) => {
+      const res = await axiosClient.delete('/users/me/histories')
+      return res.data
+    },
+    onError: console.error,
+    onSuccess: data => {
+      queryClient.invalidateQueries({ queryKey: ['histories'] })
     },
   })
 }
@@ -51,7 +79,19 @@ export function useGetUserAlerts() {
   })
 }
 
-
+export function useDeleteUserAlert() {
+    const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: async (data) => {
+      const res = await axiosClient.delete('/alerts', data)
+      return res.data
+    },
+    onError: console.error,
+    onSuccess: data => {
+      queryClient.invalidateQueries({ queryKey: ['alerts'] })
+    },
+  })
+}
 
 export function useGetShops() {
      return useQuery<any>({
@@ -60,5 +100,46 @@ export function useGetShops() {
       const res = await axiosClient.get('/shops')
       return res.data
     },
+  })
+}
+
+export function useGetShop(shop_id: number, slug: string) {
+     return useQuery<any>({
+    queryKey: ['shops', shop_id, slug],
+    queryFn: async () => {
+      const res = await axiosClient.get(`/shops/${shop_id}/${slug}`)
+      return res.data
+    },
+  })
+}
+
+export function useGetUserReports() {
+     return useQuery<any>({
+    queryKey: ['reports'],
+    queryFn: async () => {
+      const res = await axiosClient.get('/reports')
+      return res.data
+    },
+  })
+}
+
+export function useGetUserTickets() {
+     return useQuery<any>({
+    queryKey: ['tickets'],
+    queryFn: async () => {
+      const res = await axiosClient.get('/tickets')
+      return res.data
+    },
+  })
+}
+
+export function useGetAutoComplete(keyword: string) {
+       return useQuery<any>({
+    queryKey: ['autocomplete', keyword],
+    queryFn: async () => {
+      const res = await axiosClient.get(`/search/autocomplete?keyword=${keyword}`)
+      return res.data
+    },
+    enabled: keyword.trim().length > 0,
   })
 }
