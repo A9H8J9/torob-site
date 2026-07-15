fb6824e92a2193862d003fd0ceb27382bfea3f90
amirhosein <140063353+A9H8J9@users.noreply.github.com>
2026-07-15 17:18:53 +0330

    add api to favorite

--- app/(main)/(account)/user/favorites/page.tsx
+++ app/(main)/(account)/user/favorites/page.tsx
@@ -1,3 +1,14 @@
+'use client'
+import { useGetUserFavorites } from "@/lib/apis"
+
 export default function Favorites() {
-  return "favorites";
+  const {data, isPending, error} = useGetUserFavorites()
+   return data?.length > 0 ?  (
+    <p>favorites</p>
+    ) : (
+      <div className="w-full h-full flex flex-col items-center py-4 bg-[#ffffff] dark:bg-[#212b36]">
+        <img className="w-96" src='https://assets.torob.com/public/main/images/empty_likes.PNG' />
+        <p className="text-center text-sm mt-6 max-w-xs text-[#919eab]">محصولات محبوب خود را انتخاب کنید تا بعدا راحت‌تر پیدایشان کنید.</p>
+      </div>
+    )
 }
